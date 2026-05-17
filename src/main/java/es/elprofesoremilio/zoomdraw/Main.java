b64ff7981aa8013352e19bef37a56443ca82f538
elprofesoremilio <elprofesoremilio@gmail.com>
2026-05-17 15:31:36 +0200

    ajusto dos arranques para wayland y no wayland

--- src/main/java/es/elprofesoremilio/zoomdraw/Main.java
+++ src/main/java/es/elprofesoremilio/zoomdraw/Main.java
@@ -17,15 +17,6 @@ public class Main {
         // al evitar que intente usar aceleración por hardware que Wayland bloquea
         System.setProperty("sun.java2d.opengl", "false");
         java.awt.Toolkit.getDefaultToolkit();
-        Application.launch(es.elprofesoremilio.zoomdraw.app.UbuntuWaylandIntegrator.class, args);
+        Application.launch(es.elprofesoremilio.zoomdraw.app.AppLauncher.class, args);
     }
-
-    /*
-    Funciona genial pero necesito algunos ajustes:
-    - la escena se abre siempre en el mismo monitor, independientemente de
-    donde esté el cursor, haz que detecte en qué monitor estamos y que se abra ahí.
-    - una vez abierta la escena se cierra bien si pulso CTRL+1, pero para cerrarse con la tecla ESC
-    tengo que estar con el foco en dicha escena. Eso está bien, es justo lo que queremos, el
-    problema es que la escena no coge el foco al pulsar CTRL+1 cuando se activa el modo anotación. Haz que lo coja.
-     */
 }
---
+++ src/main/java/es/elprofesoremilio/zoomdraw/MainWayland.java
@@ -0,0 +1,20 @@
+package es.elprofesoremilio.zoomdraw;
+import javafx.application.Application;
+
+/**
+ * Entry point for the ZoomDraw application.
+ */
+public class MainWayland {
+    public static void main(String[] args) {
+        Application.launch(es.elprofesoremilio.zoomdraw.app.UbuntuWaylandIntegrator.class, args);
+    }
+
+    /*
+    Funciona genial pero necesito algunos ajustes:
+    - la escena se abre siempre en el mismo monitor, independientemente de
+    donde esté el cursor, haz que detecte en qué monitor estamos y que se abra ahí.
+    - una vez abierta la escena se cierra bien si pulso CTRL+1, pero para cerrarse con la tecla ESC
+    tengo que estar con el foco en dicha escena. Eso está bien, es justo lo que queremos, el
+    problema es que la escena no coge el foco al pulsar CTRL+1 cuando se activa el modo anotación. Haz que lo coja.
+     */
+}
